064543510e7bbd2de4969962c02b4a3148330637
Nathan Clarke <CaptainCow95@gmail.com>
2014-11-19 00:08:34 -0700

    Added the Mono.Options library
    Added the Mono.Options library to help with command line argument
    parsing.

---
+++ DatabaseMaster/MasterNode.cs
@@ -0,0 +1,16 @@
+﻿namespace DatabaseMaster
+{
+    public static class MasterNode
+    {
+        private static int _port;
+
+        public static void Start(int port)
+        {
+            _port = port;
+        }
+
+        public static void Stop()
+        {
+        }
+    }
+}
--- DatabaseMaster/Program.cs
+++ DatabaseMaster/Program.cs
@@ -1,4 +1,7 @@
 ﻿using Logging;
+using Mono.Options;
+using System;
+using System.Collections.Generic;
 using System.Collections.Specialized;
 using WebServer;
 
@@ -8,12 +11,50 @@ namespace DatabaseMaster
     {
         private static void Main(string[] args)
         {
+            int port = 12345;
+            OptionSet optionSet = new OptionSet()
+            {
+                {"p|port=", "The port the program should run on.", (int e) => port = e}
+            };
+
+            List<string> extras;
+            try
+            {
+                extras = optionSet.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                extras = new List<string>();
+                Console.WriteLine(e.Message);
+            }
+
+            if (extras.Count > 0)
+            {
+                Console.Write("Unknown command line options: ");
+                bool first = true;
+                foreach (var item in extras)
+                {
+                    if (first)
+                    {
+                        first = false;
+                    }
+                    else
+                    {
+                        Console.Write(", ");
+                    }
+
+                    Console.Write(item);
+                }
+            }
+
             Logger.Init(string.Empty, "master");
-            WebInterface.Start(12345, WebInterfaceRequestReceived);
+            MasterNode.Start(port);
+            WebInterface.Start(port + 1, WebInterfaceRequestReceived);
 
             // Rest of code goes here.
 
             WebInterface.Stop();
+            MasterNode.Stop();
         }
 
         private static string WebInterfaceRequestReceived(string page, NameValueCollection queryString)
