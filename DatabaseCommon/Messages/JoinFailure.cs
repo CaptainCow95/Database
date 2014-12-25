c07c2f6fa8465200c4723e19df9567e457550639
Nathan Clarke <CaptainCow95@gmail.com>
2014-12-24 17:12:48 -0700

    Added two more message types
    Added the other two message types that were in the MessageType enum.

---
+++ DatabaseCommon/Messages/JoinFailure.cs
@@ -0,0 +1,29 @@
+﻿namespace Database.Common.Messages
+{
+    public class JoinFailure : BaseMessageData
+    {
+        private string _reason;
+
+        public JoinFailure(string reason)
+        {
+            _reason = reason;
+        }
+
+        public JoinFailure(byte[] data, int index)
+        {
+            _reason = ByteArrayHelper.ToString(data, ref index);
+        }
+
+        public string Reason { get { return _reason; } }
+
+        public override byte[] EncodeInternal()
+        {
+            return ByteArrayHelper.ToBytes(_reason);
+        }
+
+        protected override int GetMessageTypeId()
+        {
+            return (int)MessageType.JoinFailure;
+        }
+    }
+}
---
+++ DatabaseCommon/Messages/JoinSuccess.cs
@@ -0,0 +1,29 @@
+﻿namespace Database.Common.Messages
+{
+    public class JoinSuccess : BaseMessageData
+    {
+        private bool _primaryController;
+
+        public JoinSuccess(bool primaryController)
+        {
+            _primaryController = primaryController;
+        }
+
+        public JoinSuccess(byte[] data, int index)
+        {
+            _primaryController = ByteArrayHelper.ToBoolean(data, ref index);
+        }
+
+        public bool PrimaryController { get { return _primaryController; } }
+
+        public override byte[] EncodeInternal()
+        {
+            return ByteArrayHelper.ToBytes(_primaryController);
+        }
+
+        protected override int GetMessageTypeId()
+        {
+            return (int)MessageType.JoinSuccess;
+        }
+    }
+}
