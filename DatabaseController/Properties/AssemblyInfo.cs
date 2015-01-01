d141d522f15bb6ccb198d17a096999d6dfcce7ec
Nathan Clarke <CaptainCow95@gmail.com>
2015-01-01 00:50:16 -0800

    Updated AssemblyInfo
    Updated the assembly info from the old names to the new names.

--- DatabaseController/Properties/AssemblyInfo.cs
+++ DatabaseController/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DatabaseMaster")]
+[assembly: AssemblyTitle("DatabaseController")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DatabaseMaster")]
+[assembly: AssemblyProduct("DatabaseController")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- DatabaseStorage/Properties/AssemblyInfo.cs
+++ DatabaseStorage/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DatabaseClient")]
+[assembly: AssemblyTitle("DatabaseStorage")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DatabaseClient")]
+[assembly: AssemblyProduct("DatabaseStorage")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
